de8a255ab39fe503e85d7526806c1789c0566391
Bruce Martin <brucejmartin.bm@gmail.com>
2026-07-17 01:40:51 -0400

    added magnet class, updated levels to reflect class change, added keyboard io for movement

--- src/game.py
+++ src/game.py
@@ -5,6 +5,7 @@ from PySide6.QtWidgets import QWidget, QMainWindow
 from PySide6.QtCore import Qt
 import os
 from src.level import Level
+from src.magnets import Magnet
 
 class GameWindow(QMainWindow):
 	def __init__(self):
@@ -15,15 +16,21 @@ class GameWindow(QMainWindow):
 
 		self.game_widget = GameWidget()
 		self.setCentralWidget(self.game_widget)
+		self.adjustSize()
 
 class GameWidget(QWidget):
 	def __init__(self):
 		super().__init__()
 
+		self.setFixedSize(1200, 720)
+		self.setFocusPolicy(Qt.StrongFocus)
+		self.setFocus()
+
 		#assets
 		self.background = QPixmap("assets/images/background_brown.png")
 		self.ball = QPixmap("assets/images/ball_blue_small.png")
 		self.hole = QPixmap("assets/images/hole.png")
+		self.magnet = Magnet()
 
 		self.level = Level(self)
 		self.level.new_level()
@@ -39,3 +46,23 @@ class GameWidget(QWidget):
 		painter.drawPixmap(0, 0, scaled_background)
 		painter.drawPixmap(self.hole_x, self.hole_y, self.hole)
 		painter.drawPixmap(self.ball_x, self.ball_y, self.ball)
+		print("painting")
+		painter.drawPixmap(self.magnet.x, self.magnet.y, self.magnet.sprite)
+
+	# game.py
+	def keyPressEvent(self, event):
+		print("key received")
+		if event.key() == Qt.Key_W:
+			self.magnet.move_up()
+		elif event.key() == Qt.Key_S:
+			self.magnet.move_down()
+		elif event.key() == Qt.Key_A:
+			self.magnet.move_left()
+		elif event.key() == Qt.Key_D:
+			self.magnet.move_right()
+		elif event.key() == Qt.Key_Right:
+			self.magnet.rotate_right()
+		elif event.key() == Qt.Key_Left:
+			self.magnet.rotate_left()
+		self.update()
+		print("update requested")
--- src/level.py
+++ src/level.py
@@ -1,7 +1,7 @@
 # level.py
 import random
 import math
-
+from PySide6.QtCore import QRect
 
 class Level:
 
@@ -11,6 +11,7 @@ class Level:
 	def new_level(self):
 		self.place_hole()
 		self.place_ball()
+		self.place_magnet()
 
 	def place_hole(self):
 		#placement limits
@@ -37,3 +38,39 @@ class Level:
 
 			if distance >= 450:
 				break
+
+	def place_magnet(self):
+		max_x = self.game.width() - self.game.magnet.width
+		max_y = self.game.height() - self.game.magnet.height
+
+		ball_rect = QRect(
+			self.game.ball_x,
+			self.game.ball_y,
+			self.game.ball.width(),
+			self.game.ball.height()
+		)
+
+		hole_rect = QRect(
+			self.game.hole_x,
+			self.game.hole_y,
+			self.game.hole.width(),
+			self.game.hole.height()
+		)
+
+		for _ in range(100):
+			magnet_x = random.randint(0, max_x)
+			magnet_y = random.randint(0, max_y)
+
+			magnet_rect = QRect(
+				magnet_x,
+				magnet_y,
+				self.game.magnet.width,
+				self.game.magnet.height
+			)
+
+			if not magnet_rect.intersects(ball_rect) and \
+			   not magnet_rect.intersects(hole_rect):
+
+				self.game.magnet.x = magnet_x
+				self.game.magnet.y = magnet_y
+				return
